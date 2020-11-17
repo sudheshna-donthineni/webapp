f1b0a1d9b2d60896f2f6cdf2f7d22decfd766c63
sudonthineni <sudonthineni@users.noreply.github.com>
2020-11-17 14:02:58 -0800

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDq5hP2zXTSfNofee82jhgyN2H1EClS3Qs",
+  authDomain: "web-app-6a382.firebaseapp.com",
+  databaseURL: "https://web-app-6a382.firebaseio.com",
+  projectId: "web-app-6a382",
+  storageBucket: "web-app-6a382.appspot.com",
+  messagingSenderId: "789572959936",
+  appId: "1:789572959936:web:7232c810d8432ec3663725",
+  measurementId: "G-GE2WWM13PB",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
